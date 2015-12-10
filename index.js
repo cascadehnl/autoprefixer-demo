6d982232a6b219018f4240da51fe6b5e4e86df26
bradbaris <bradbaris@gmail.com>
2015-12-09 21:22:32 -1000

    Added basic custom plugin example

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ var atImport = require("postcss-import")
 var autoprefixer = require("autoprefixer")
 var nested = require("postcss-nested")
 
+// Example custom PostCSS plugin (inlined)
+var remove = postcss.plugin('remove', function (opts) {
+
+    opts = opts || {};
+
+    // Work with options here
+
+    return function (css, result) {
+
+        // Transform CSS AST here
+
+        css.walkDecls(opts, function (decl) {
+            decl.remove();
+        });
+    };
+});
+
 // Read in CSS to process
 var css = fs.readFileSync("src/style_nested.css", "utf8")
 
 // Process CSS with PostCSS
-var output = postcss([autoprefixer, nested])
+var output = postcss([autoprefixer, nested, remove('opacity')])
   .use(atImport())
   .process(css, {
     from: "./src/style_nested.css",
